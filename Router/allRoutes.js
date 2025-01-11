641cd2afafa077befb341f8a0e44aae01c56870d
Nabil-FRA <nabil.ait.radi1997@gmail.com>
2025-01-11 11:27:17 +0100

    Mise à jour des fichiers de routage de l'appli pour les différents rôles

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,13 +2,13 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Signin", "Connexion", "/Pages/Auth/Signin.html", "/JAVAScript/auth/Signin.js"),
-    new Route("/Signup", "Inscription", "/Pages/Auth/Signup.html", "/JAVAScript/auth/Signup.js"),
-    new Route("/Account", "Mon compte", "/Pages/Auth/Account.html"),
-    new Route("/Contact", "Contact", "/Pages/Contact.html", "/JAVAScript/contact.js"),
-    new Route("/Motdepasse", "Motdepasse", "/Pages/Auth/mot de pass.html", "/JAVAScript/auth/mot de passe.js"),
-    new Route("/Covoiturage", "Covoiturage", "/Pages/Covoiturage.html", "/JAVAScript/covoiturage.js"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/Signin", "Connexion", "/Pages/Auth/Signin.html",[], "/JAVAScript/auth/Signin.js" ),
+    new Route("/Signup", "Inscription", "/Pages/Auth/Signup.html",["disconnected"], "/JAVAScript/auth/Signup.js"),
+    new Route("/Account", "Mon compte", "/Pages/Auth/Account.html", ["client", "admin"]),
+    new Route("/Contact", "Contact", "/Pages/Contact.html",[], "/JAVAScript/contact.js"),
+    new Route("/Motdepasse", "Motdepasse", "/Pages/Auth/mot de pass.html",[], "/JAVAScript/auth/mot de passe.js"),
+    new Route("/Covoiturage", "Covoiturage", "/Pages/Covoiturage.html",[], "/JAVAScript/covoiturage.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
