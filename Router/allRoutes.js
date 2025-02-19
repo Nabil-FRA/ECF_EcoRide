e9552f313fb2ccf9c30ad9ab84731f1e6ebb320c
Nabil-FRA <nabil.ait.radi1997@gmail.com>
2025-02-19 18:21:10 +0100

    Modification de allroute et routeur

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 
 // Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html",[]),
+    new Route("/", "Accueil", "/Pages/home.html",[]),
     new Route("/Signin", "Connexion", "/Pages/Auth/Signin.html",[], "/JAVAScript/auth/Signin.js"),
     new Route("/Signup", "Inscription", "/Pages/Auth/Signup.html",[], "/JAVAScript/auth/Signup.js"),
     new Route("/Account", "Mon compte", "/Pages/Auth/Account.html",[], "/JAVAScript/auth/Acount.js"),
